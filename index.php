c56f82f11d2d4ac012e5b07c1edb196fb71fa7da
GabrielDSousa <gabrielramos.email@gmail.com>
2023-11-02 00:26:46 +0000

    Conditionals and Booleans

--- index.php
+++ index.php
@@ -3,15 +3,31 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
+	<title>Demo</title>
+	<style>
+		body {
+			background-color: #000;
+			color: #fff;
+			font-family: sans-serif, Arial, Helvetica;
+			font-size: 1.2rem;
+			text-align: center;
+			height: 100vh;
+			display: grid;
+			margin: 0;
+			place-items: center;
+		}
+	</style>
 </head>
 <body>
-	<h1>
-		<?php
-			$greeting = "Hello";
-			
-			echo "$greeting World!";
-		?>
-	</h1>
+	<?php 
+		$name = "Dark Matter";
+		$read = true;
+		$message = "You haven't read $name";
+
+		if($read) {
+			$message = "You have read $name";
+		}
+	?>
+	<h1><?= $message ?>.</h1>
 </body>
 </html>
