c37cf0a796f37b6abcfe6ab693aa016b21598382
GabrielDSousa <gabrielramos.email@gmail.com>
2023-11-02 00:54:45 +0000

    Associative Arrays

--- index.php
+++ index.php
@@ -11,11 +11,24 @@
 			color: #fff;
 			font-family: sans-serif, Arial, Helvetica;
 			font-size: 1.2rem;
-			text-align: center;
 			height: 100vh;
 			display: grid;
 			margin: 0;
 			place-items: center;
+		}
+		li {
+			list-style: none;
+			margin: 0.8rem 0;
+			display: flex;
+			flex-direction: column;
+			text-align: left;
+		}
+		ul {
+			padding: 0;
+		}
+		li small {
+			font-size: 0.8rem;
+			color: #ccc;
 		}
 	</style>
 </head>
@@ -25,20 +38,45 @@
 		<h1>Recommended Anime</h1>
 
 		<?php
-			$animes = [
-				'One Piece',
-				'Gundam Iron Bloded Orphans',
-				'Fullmetal Alchemist Brotherhood',
-				'Code Geass'
+			$anime = [
+				[
+					'title' => 'One Piece',
+					'year' => 1999,
+					'genre' => 'Action',
+					'author' => 'Eiichiro Oda'
+				],
+				[
+					'title' =>'Gundam Iron Bloded Orphans',
+					'year' => 2015,
+					'genre' => 'Action',
+					'author' => 'Yoshiyuki Tomino'
+				],
+				[
+					'title' => 'Fullmetal Alchemist Brotherhood',
+					'year' => 2009,
+					'genre' => 'Action',
+					'author' => 'Hiromu Arakawa'
+				],
+				[
+					'title' => 'Code Geass',
+					'year' => 2006,
+					'genre' => 'Action',
+					'author' => 'Goro Taniguchi'
+				]
 			];
 		?>
 
 		<ul>
-			<?php foreach ($anime as $title) : ?>
-				<li><?= $title ?></li>
+			<?php foreach ($anime as $item) : ?>
+				<li>
+					<?= $item['title'] ?> - <?= $item['year'] ?>
+					<small><?= $item['genre'] ?> - <?= $item['author'] ?></small>
+				</li>
 			<?php endforeach; ?>
 		</ul>
 	</div>
+
+	<p>Currently re-watching: <?= $anime[0]['title']?></p>
 </body>
 
 </html>
