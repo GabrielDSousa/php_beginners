d9c0a2e9a88da13535e3aec0a4a33ee292b7ab86
GabrielDSousa <gabrielramos.email@gmail.com>
2023-11-04 00:05:00 +0000

    Page links

--- data.php
+++ data.php
@@ -1,35 +1,3 @@
 <php
 <?php
 
-$anime = [
-	[
-		"title" => "One Piece",
-		"year" => 1999,
-		"genre" => "Action",
-		"writer" => "Eiichiro Oda"
-	],
-	[
-		"title" => "Gundam Iron Bloded Orphans",
-		"year" => 2015,
-		"genre" => "Action",
-		"writer" => "Mari Okada"
-	],
-	[
-		"title" => "Fullmetal Alchemist Brotherhood",
-		"year" => 2009,
-		"genre" => "Action",
-		"writer" => "Hiromu Arakawa"
-	],
-	[
-		"title" => "Code Geass",
-		"year" => 2006,
-		"genre" => "Action",
-		"writer" => "Goro Taniguchi"
-	],
-	[
-		"title" => "Anohana",
-		"year" => 2011,
-		"genre" => "Drama",
-		"writer" => "Mari Okada"
-	]
-];
--- index.php
+++ index.php
@@ -1,9 +1,3 @@
 <?php
 
-require 'data.php';
-
-$filteredAnime = array_filter($anime, function ($item) {
-	return $item['writer'] === 'Mari Okada';
-});
-
 require 'index.view.php';
